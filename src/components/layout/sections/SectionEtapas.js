5e34c9b201a2331bba3663c5b25b30c30db020f0
Mayara Martinello Bastos <maybastos2021@gmail.com>
2025-02-19 17:48:40 -0300

    style section etapas

--- src/components/layout/sections/SectionEtapas.js
+++ src/components/layout/sections/SectionEtapas.js
@@ -1,30 +1,29 @@
 import styles from "./SectionEtapas.module.css";
 import stylesContainer from "../Container.module.css";
+import stylesBeneficios from "./SectionBeneficios.module.css";
 
 function SectionEtapas() {
   return (
     <section className={`${stylesContainer.section}`}>
-      <div className="sc-eqUAAy dzXXvd">
+      <div className={`${stylesBeneficios.div_linhas_title}`}>
         <div
           aria-label="Linha a esquerda do titulo"
-          className="sc-dcJsrY iNzVsI"
+          className={`left-0 ${stylesBeneficios.div_linha}`}
         ></div>
         <h2 className="sc-fqkvVR ldfcuN">Etapas</h2>
         <div
           aria-label="Linha a direita do titulo"
-          className="sc-iGgWBj jDNXdV"
+          className={`right-0 ${stylesBeneficios.div_linha}`}
         ></div>
       </div>
-      <p className="sc-kAkpmW lmUHeL">
-        Conheça as etapas do nosso processo seletivo
-      </p>
-      <div className="sc-gFVvzn cQzpBG">
-        <div className="sc-bXCLTC jAskVB">
-          <ol className="sc-eeDRCY hvIbGv">
+      <p>Conheça as etapas do nosso processo seletivo</p>
+      <div className="mt-[8rem] mx-auto max-w-[106.7rem] px-[1.6rem] ">
+        <div className={`${styles.div_ol}`}>
+          <ol>
             <li>
-              <div className="opacity-0 translate-y-2.5 transform">
+              <div className={`${styles.div_li}`}>
                 <span>1</span>
-                <div className="opacity-0 translate-y-2.5 transform">
+                <div className={`${styles.div_text_card}`}>
                   <h3 className="sc-bmzYkS jghSuU">Pré-inscrição</h3>
                   <p className="sc-iHGNWf esbTeF">
                     Inicie a sua inscrição preenchendo o nosso formulário
@@ -33,9 +32,9 @@ function SectionEtapas() {
               </div>
             </li>
             <li>
-              <div className="opacity-0 translate-y-2.5 transform">
+              <div className={`${styles.div_li}`}>
                 <span>2</span>
-                <div className="opacity-0 translate-y-2.5 transform">
+                <div className={`${styles.div_text_card}`}>
                   <h3 className="sc-bmzYkS jghSuU">Análise do formulário</h3>
                   <p className="sc-iHGNWf esbTeF">
                     O nosso time irá analisar suas informações
@@ -44,9 +43,9 @@ function SectionEtapas() {
               </div>
             </li>
             <li>
-              <div className="opacity-0 translate-y-2.5 transform">
+              <div className={`${styles.div_li}`}>
                 <span>3</span>
-                <div className="opacity-0 translate-y-2.5 transform">
+                <div className={`${styles.div_text_card}`}>
                   <h3 className="sc-bmzYkS jghSuU">Desafio técnico</h3>
                   <p className="sc-iHGNWf esbTeF">
                     Após a análise você receberá um desafio prático
@@ -55,9 +54,9 @@ function SectionEtapas() {
               </div>
             </li>
             <li>
-              <div className="opacity-0 translate-y-2.5 transform">
+              <div className={`${styles.div_li}`}>
                 <span>4</span>
-                <div className="opacity-0 translate-y-2.5 transform">
+                <div className={`${styles.div_text_card}`}>
                   <h3 className="sc-bmzYkS jghSuU">Entrevista</h3>
                   <p className="sc-iHGNWf esbTeF">
                     Nessa fase iremos bater um papo para que possamos conhecer
